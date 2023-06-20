4c239d1a820cc140cf3cd69f8ef49228a3078b68
sharanya chandu <you@example.com>
2023-06-20 23:44:49 +0530

    auto save

--- vars.tf
+++ vars.tf
@@ -1,8 +1,1 @@
-variable "VPC_CIDR" {}
 variable "ENV" {}
-variable "AZ" {}
-variable "PUBLIC_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_CIDR" {}
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_CIDR" {}
-variable  "DEFAULT_VPC_RT" {}
