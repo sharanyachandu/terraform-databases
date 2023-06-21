013a97cb3a984e6d93d9f55e007cec6fd3c4b20e
sharanya chandu <you@example.com>
2023-06-21 23:38:29 +0530

    auto save

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 module "docdb" {
-  source     = "./vendor/modules/docdb"
-  ENV        =  var.ENV
+  source                    = "./vendor/modules/docdb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  ENV                       = var.ENV
+  DOCDB_PORT                = var.DOCDB_PORT
+  DOCDB_INSTANCE_CLASS      = var.DOCDB_INSTANCE_CLASS
+  DOCDB_INSTANCE_COUNT      = var.DOCDB_INSTANCE_COUNT
 }
 
+
 module "redis" {
-  source     = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
-  ENV        = var.ENV
+  source                       = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  ENV                          = var.ENV
+  ELASTIC_CACHE_PORT           = var.ELASTIC_CACHE_PORT
+  ELASTIC_CACHE_NODE_TYPE      = var.ELASTIC_CACHE_NODE_TYPE
+  ELASTIC_CACHE_NODE_COUNT     = var.ELASTIC_CACHE_NODE_COUNT
+  ELASTIC_CACHE_ENGINE_VERSION = var.ELASTIC_CACHE_ENGINE_VERSION
 }
--- provider.tf
+++ provider.tf
@@ -2,7 +2,6 @@ provider "aws" {
     region  =   "us-east-1"
 }
 
-# Values with in the backend config are supplied from the environment specific
 terraform {
   backend "s3" {}
 }
