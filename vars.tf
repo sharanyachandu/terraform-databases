bc5e89ca78b0dd888a2ab8bb0d91dcb07cf09f3a
sharanya chandu <you@example.com>
2023-06-24 14:55:34 +0530

    auto save

--- vars.tf
+++ vars.tf
@@ -1,14 +1,3 @@
 variable "ENV" {}
 
 
-
-# DocDB Variables 
-variable "DOCDB_PORT" {}
-variable "DOCDB_INSTANCE_CLASS" {}
-variable "DOCDB_INSTANCE_COUNT" {}
-
-# Elasticcache Variables 
-variable "ELASTIC_CACHE_PORT" {}
-variable "ELASTIC_CACHE_NODE_TYPE" {}
-variable "ELASTIC_CACHE_NODE_COUNT" {}
-variable "ELASTIC_CACHE_ENGINE_VERSION" {}
