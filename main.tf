cf6f045fd4b9968a5d402068af780fe3ee7e4fe5
sharanya chandu <you@example.com>
2023-06-27 00:11:54 +0530

    auto save

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "docdb" {
   source                    = "./vendor/modules/docdb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ENV                       = var.ENV
- # DOCDB_PORT                = var.DOCDB_PORT
- # DOCDB_INSTANCE_CLASS      = var.DOCDB_INSTANCE_CLASS
- #DOCDB_INSTANCE_COUNT      = var.DOCDB_INSTANCE_COUNT
+  DOCDB_PORT                = var.DOCDB_PORT
+  DOCDB_INSTANCE_CLASS      = var.DOCDB_INSTANCE_CLASS
+  DOCDB_INSTANCE_COUNT      = var.DOCDB_INSTANCE_COUNT
 }
 
