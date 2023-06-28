d02f7b61ab67e95bb081935edeadd85835d0951f
sharanya chandu <you@example.com>
2023-06-29 00:45:48 +0530

    auto save

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ module "docdb" {
   # DOCDB_INSTANCE_COUNT      = var.DOCDB_INSTANCE_COUNT
 }
 
+module "redis" {
+  source                       = "./vendor/modules/redis"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
+  ENV                          = var.ENV
+  ELASTIC_CACHE_PORT           = var.ELASTIC_CACHE_PORT
+  ELASTIC_CACHE_NODE_TYPE      = var.ELASTIC_CACHE_NODE_TYPE
+  ELASTIC_CACHE_NODE_COUNT     = var.ELASTIC_CACHE_NODE_COUNT
+  ELASTIC_CACHE_ENGINE_VERSION = var.ELASTIC_CACHE_ENGINE_VERSION
+}
--- vars.tf
+++ vars.tf
@@ -3,3 +3,10 @@ variable "ENV" {}
 #variable "DOCDB_PORT" {}
 #variable "DOCDB_INSTANCE_CLASS" {}
 #variable "DOCDB_INSTANCE_COUNT" {}
+
+
+# Elasticcache Variables 
+variable "ELASTIC_CACHE_PORT" {}
+variable "ELASTIC_CACHE_NODE_TYPE" {}
+variable "ELASTIC_CACHE_NODE_COUNT" {}
+variable "ELASTIC_CACHE_ENGINE_VERSION" {}
